d61e62b8e15867cca6a4648e00c365ca7a32e3d3
Vadim <Vadim>
2018-04-08 19:42:36 +0300

    Fifth

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JAVA_OPTS -jar target/dependency/webapp-runner.jar --port $PORT target/*.war
+web: java -jar target/phone-bookproject-1.0.jar
--- src/main/java/com/phonebook/Main.java
+++ src/main/java/com/phonebook/Main.java
@@ -1,16 +1,14 @@
 package com.phonebook;
 
-import com.phonebook.config.WebSecurityConfig;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PhoneBookProjectApp {
+public class Main
+{
 
 	public static void main(String[] args) {
-		SpringApplication.run(PhoneBookProjectApp.class, args);
+		SpringApplication.run(Main.class, args);
 	}
-
 
 }
